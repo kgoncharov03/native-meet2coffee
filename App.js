de303ec5f71606147c8d623005a327b3fe93943c
Artem Gazizov <russianunsignedlongint@gmail.com>
2023-01-18 19:25:11 -0500

    fixed token storage

--- App.js
+++ App.js
@@ -1,39 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import AppNavigator from './navigation/AppNavigator';
+import React, { useEffect, useState } from 'react'
+import AppNavigator from './navigation/AppNavigator'
 import LoginScreen from './screens/LoginScreen';
 
 import AuthContext from './auth/context';
 
-import authStorage from './auth/storage';
+import authStorage from "./auth/storage"
 
 import apiClient from './api/client';
 
-function App() {
-    const [user, setUser] = useState(true);
 
-    const restoreToken = async () => {
-        const token = await authStorage.getToken();
-        if (!token) return;
+ function App() {
 
-        const result = await apiClient.get('/User', {}, {});
 
-        console.log(result);
-        console.log('RESTORED TOKEN', token);
+  const [user,setUser] = useState(false);
 
-        if (!result.ok) return;
+  const restoreToken = async()=>{
 
-        setUser(token);
-    };
+      const token = await authStorage.getToken();
+      //console.log(token)
+      if(!token) return;
+    
+      //console.log("RESTORED TOKEN",token)
 
-    useEffect(() => {
-        restoreToken();
-    }, []);
+      setUser(token)
+      
+  }
+  
+  useEffect(() => {
+  restoreToken();
+  },[])
 
-    return (
-        <AuthContext.Provider value={{ user, setUser }}>
-            {user ? <AppNavigator /> : <LoginScreen />}
-        </AuthContext.Provider>
-    );
+  return (
+
+     <AuthContext.Provider value ={{user,setUser}}> 
+
+            {user ? <AppNavigator/> : <LoginScreen/>}
+
+      </AuthContext.Provider>
+
+ )
+  
 }
 
 export default App;
